1c7ac6eee09795367aeb8e343012c884768c967e
Oleksandr Sukhinin <syhinin@gmail.com>
2024-01-21 17:23:08 +0100

    update Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+] as Record<string, string>[];
 
 export const Navbar = () => {
   return (
@@ -13,7 +18,22 @@ export const Navbar = () => {
             height={27}
             alt="logo"
           />
+          <p className="nav-logo">
+            Prise <span className="text-primary">Wise</span>
+          </p>
         </Link>
+        <div className="flex item-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              width={28}
+              height={28}
+              alt={icon.alt}
+              className="object-contain"
+            />
+          ))}
+        </div>
       </nav>
     </header>
   );
