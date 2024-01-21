943f6fc4287e98a3d09536ccda5ef7ab45472d6f
Oleksandr Sukhinin <syhinin@gmail.com>
2024-01-21 14:23:33 +0100

    add Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+        </Link>
+      </nav>
+    </header>
+  );
 };
